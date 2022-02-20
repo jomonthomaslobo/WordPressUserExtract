21a7936c4ba92ff5de34454fff706502e3148a10
Jomon Thomas Lobo <jomon@akiraplc.com>
2022-02-20 18:00:05 +0530

    Added exception handler

--- extract.py
+++ extract.py
@@ -13,68 +13,68 @@ print("for any misuse.")
 print("------------------------------------------------------------------------------------------------")
 n = len(sys.argv)
 #print(sys.argv[2])
-
-if (n == 1):
-    print("Arguments are missing Usage: python3 extract.py --url <url>")
-    sys.exit(1) 
-if(n > 3):
-    print("Too many arguments Usage: python3 extract.py --url <url>")
-    sys.exit(1)
-if(sys.argv[1] == "--url"):
-    if(len(sys.argv[2])==0):
+try:
+    if (n == 1):
         print("Arguments are missing Usage: python3 extract.py --url <url>")
+        sys.exit(1) 
+    if(n > 3):
+        print("Too many arguments Usage: python3 extract.py --url <url>")
         sys.exit(1)
-    else:
-        print(" Connecting to " + sys.argv[2]) 
-        initialUserLimit = 100
-        x=range(initialUserLimit)
-        print(" Checking First "+ str(initialUserLimit) +" users")
-        for i in x:
-            url = sys.argv[2] + "/?author=" + str(i)
-            #print(url)
+    if(sys.argv[1] == "--url"):
+        if(len(sys.argv[2])==0):
+            print("Arguments are missing Usage: python3 extract.py --url <url>")
+            sys.exit(1)
+        else:
+            print(" Connecting to " + sys.argv[2]) 
+            initialUserLimit = 100
+            x=range(initialUserLimit)
+            print(" Checking First "+ str(initialUserLimit) +" users")
+            for i in x:
+                url = sys.argv[2] + "/?author=" + str(i)
+                #print(url)
             
-            r = requests.get(url)
-            if(r.status_code == 200):
-               if(r.url.split("/")[-2] != "author"):  
-                 print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
-               else :
-                    print("User extraction not available in this site")
+                r = requests.get(url)
+                if(r.status_code == 200):
+                    if(r.url.split("/")[-2] != "author"):  
+                        print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
+                    else :
+                         print("User extraction not available in this site")
+                         sys.exit(1)
+                else:
+                    print("User " + str(i) + " does not exist")
+            print       ("------------------------------------------------------------------------------------------------")
+            print("Checking for more users")
+            # for(i=initialUserLimit+1;i<=initialUserLimit+100;i++):
+            initialUserLimit+=100
+            i=initialUserLimit+1
+            y=range(initialUserLimit,initialUserLimit+100)
+            errorCount=0
+            for j in y:
+                url = sys.argv[2] + "/?author=" + str(j)
+                #print(url)
+                r = requests.get(url)
+                if(r.status_code == 200):
+                    if(r.url.split("/")[-2] != "author"):  
+                     print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
+                    else :
+                            print("User extraction not available in this site")
+                            sys.exit(1)
+                else:
+                    print("User " + str(j) + " does not exist")
+                    errorCount+=1
+                if(r.status_code== 404):
+                    print("------------------------------------------------------------------------------------------------")
+                    print("No more users found")
+                    print("------------------------------------------------------------------------------------------------")
                     sys.exit(1)
-            else:
-                print("User " + str(i) + " does not exist")
-        print("------------------------------------------------------------------------------------------------")
-        print("Checking for more users")
-       # for(i=initialUserLimit+1;i<=initialUserLimit+100;i++):
-        initialUserLimit+=100
-        i=initialUserLimit+1
-        y=range(initialUserLimit,initialUserLimit+100)
-        errorCount=0
-        for j in y:
-            url = sys.argv[2] + "/?author=" + str(j)
-            #print(url)
-            r = requests.get(url)
-            if(r.status_code == 200):
-               if(r.url.split("/")[-2] != "author"):  
-                 print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
-               else :
-                    print("User extraction not available in this site")
+                if(errorCount==20):
+                    print("------------------------------------------------------------------------------------------------")
+                    print("No more users found")
+                    print("------------------------------------------------------------------------------------------------")
                     sys.exit(1)
-            else:
-                print("User " + str(j) + " does not exist")
-                errorCount+=1
-            if(r.status_code== 404):
                 print("------------------------------------------------------------------------------------------------")
-                print("No more users found")
-                print("------------------------------------------------------------------------------------------------")
-                sys.exit(1)
-            if(errorCount==20):
-                print("------------------------------------------------------------------------------------------------")
-                print("No more users found")
-                print("------------------------------------------------------------------------------------------------")
-                sys.exit(1)
-        print("------------------------------------------------------------------------------------------------")
         
-        print("------------------------------------------------------------------------------------------------")
+                print("------------------------------------------------------------------------------------------------")
         print("Checking for more users")
         #for(i=initialUserLimit+1;i<=initialUserLimit+100;i++):
         initialUserLimit+=100
@@ -91,7 +91,9 @@ if(sys.argv[1] == "--url"):
         print("------------------------------------------------------------------------------------------------")
        
         print("No more users found")
-
+except:
+    print("Unable to connect to the site")
+    sys.exit(1)
 
 print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
 
