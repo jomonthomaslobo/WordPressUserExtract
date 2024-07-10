c0373f0b591713355943f796a038c6a268df68b2
Jomon Lobo <jomonthomaslobo@gmail.com>
2024-07-10 15:49:55 +0530

    Optimized and improved performcance

--- extract.py
+++ extract.py
@@ -1,99 +1,56 @@
-#Extract WordPress Username 
-#Author: @jomonthomaslobo
+# Extract WordPress Usernames
+# Author: @jomonthomaslobo
 
 import sys
 import requests
-# Commandline argument count check
-print("WordPress User Extractor")
-print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
-print("Author: @jomonthomaslobo")
-print("------------------------------------------------------------------------------------------------")
-print("This application is for eductional purposes only.This Application developer is not responsible ")
-print("for any misuse.")
-print("------------------------------------------------------------------------------------------------")
-n = len(sys.argv)
-#print(sys.argv[2])
-try:
-    if (n == 1):
-        print("Arguments are missing Usage: python3 extract.py --url <url>")
+
+def print_banner():
+    print("WordPress User Extractor")
+    print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
+    print("Author: @jomonthomaslobo")
+    print("------------------------------------------------------------------------------------------------")
+    print("This application is for educational purposes only. The developer is not responsible for any misuse.")
+    print("------------------------------------------------------------------------------------------------")
+
+def validate_arguments():
+    if len(sys.argv) == 1:
+        print("Arguments are missing. Usage: python3 extract.py --url <url>")
         sys.exit(1) 
-    if(n > 3):
-        print("Too many arguments Usage: python3 extract.py --url <url>")
+    if len(sys.argv) > 3:
+        print("Too many arguments. Usage: python3 extract.py --url <url>")
         sys.exit(1)
-    if(sys.argv[1] == "--url"):
-        if(len(sys.argv[2])==0):
-            print("Arguments are missing Usage: python3 extract.py --url <url>")
-            sys.exit(1)
+    if sys.argv[1] != "--url" or not sys.argv[2]:
+        print("Arguments are missing. Usage: python3 extract.py --url <url>")
+        sys.exit(1)
+
+def extract_users(base_url):
+    print(f"Connecting to {base_url}")
+    
+    api_url = f"{base_url}/wp-json/wp/v2/users"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+
+        users = response.json()
+        if users:
+            for user in users:
+                print(f"Found User ID: {user['id']} : Username: {user['name']}")
         else:
-            print(" Connecting to " + sys.argv[2]) 
-            initialUserLimit = 100
-            x=range(initialUserLimit)
-            print(" Checking First "+ str(initialUserLimit) +" users")
-            for i in x:
-                url = sys.argv[2] + "/?author=" + str(i)
-                #print(url)
-            
-                r = requests.get(url)
-                if(r.status_code == 200):
-                    if(r.url.split("/")[-2] != "author"):  
-                        print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
-                    else :
-                         print("User extraction not available in this site")
-                         sys.exit(1)
-                else:
-                    print("User " + str(i) + " does not exist")
-            print       ("------------------------------------------------------------------------------------------------")
-            print("Checking for more users")
-            # for(i=initialUserLimit+1;i<=initialUserLimit+100;i++):
-            initialUserLimit+=100
-            i=initialUserLimit+1
-            y=range(initialUserLimit,initialUserLimit+100)
-            errorCount=0
-            for j in y:
-                url = sys.argv[2] + "/?author=" + str(j)
-                #print(url)
-                r = requests.get(url)
-                if(r.status_code == 200):
-                    if(r.url.split("/")[-2] != "author"):  
-                     print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
-                    else :
-                            print("User extraction not available in this site")
-                            sys.exit(1)
-                else:
-                    print("User " + str(j) + " does not exist")
-                    errorCount+=1
-                if(r.status_code== 404):
-                    print("------------------------------------------------------------------------------------------------")
-                    print("No more users found")
-                    print("------------------------------------------------------------------------------------------------")
-                    sys.exit(1)
-                if(errorCount==20):
-                    print("------------------------------------------------------------------------------------------------")
-                    print("No more users found")
-                    print("------------------------------------------------------------------------------------------------")
-                    sys.exit(1)
-                print("------------------------------------------------------------------------------------------------")
-        
-                print("------------------------------------------------------------------------------------------------")
-        print("Checking for more users")
-        #for(i=initialUserLimit+1;i<=initialUserLimit+100;i++):
-        initialUserLimit+=100
-        #i=initialUserLimit+1
-        y=range(initialUserLimit,initialUserLimit+100)
-        for j in y: 
-            url = sys.argv[2] + "/?author=" + str(i)
-            #print(url)
-            r = requests.get(url)
-            if(r.status_code == 200):
-                 print("Found User: " + str(i)+" : Username :"+r.url.split("/")[-2])
-            else:
-                print("User " + str(i) + " does not exist")
-        print("------------------------------------------------------------------------------------------------")
-       
-        print("No more users found")
-except:
-    print("Unable to connect to the site")
-    sys.exit(1)
+            print("No users found.")
 
-print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
+    except requests.exceptions.RequestException as e:
+        print(f"Unable to connect to the site: {e}")
+        sys.exit(1)
 
+    print("------------------------------------------------------------------------------------------------")
+    print("Extraction completed")
+    print("------------------------------------------------------------------------------------------------")
+
+if __name__ == "__main__":
+    print_banner()
+    validate_arguments()
+
+    base_url = sys.argv[2]
+    extract_users(base_url)
+
+    print("▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
